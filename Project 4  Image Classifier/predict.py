73664552292ebfd2e9b95b7aa709aa0b6899f637
Nisha <39298400+Nishamol@users.noreply.github.com>
2022-07-10 12:34:57 +0530

    Project 4 Image Classifier
    Project 4 : Part 1 (Training and prediction) and  Part 2 ( Productionizing model )

---
+++ Project 4  Image Classifier/predict.py
@@ -0,0 +1,43 @@
+import argparse
+from util import preprocessing as pp
+import json
+import tensorflow as tf
+import tensorflow_hub as hub
+import numpy as np
+from PIL import Image
+
+
+def predict(image_path,model,top_k):    
+    prediction = model.predict(np.expand_dims(image_path, axis=0))
+    result_values, result_indices = tf.math.top_k(prediction,top_k)
+    return result_values.numpy()[0], result_indices.numpy()[0]
+
+def main():
+    class_names = None
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path')
+    parser.add_argument('saved_model')
+    parser.add_argument('--top_k', type = int, action="store", dest = "top_k")
+    parser.add_argument('--category_names',action = "store", dest = "category_names")
+    args = parser.parse_args()
+    print(args)
+    top_k = 1
+    if args.top_k is not None: top_k = args.top_k
+    if args.category_names is not None: 
+        with open(args.category_names, 'r') as f:
+            class_names = json.load(f)
+    model = tf.keras.models.load_model(args.saved_model,compile = False, custom_objects={'KerasLayer':hub.KerasLayer})
+    model.load_weights('best_model').expect_partial()	
+    test_image = np.array(Image.open(args.path))
+    processed_test_image = pp.process_image(test_image)
+    prob, indices = predict(processed_test_image,model,top_k)
+    if class_names is not None:
+        classes = [class_names[str(element)] for element in indices]
+        print(list(zip(classes,prob)))
+    else:
+        print(list(zip(indices,prob)))
+    
+if __name__ == "__main__":
+    main()
+    
+
---
+++ Project 4  Image Classifier/util/preprocessing.py
@@ -0,0 +1,15 @@
+import numpy as np
+import tensorflow as tf
+
+def process_image(image):
+    image= np.squeeze(image)
+    resized_image = tf.image.resize(image, (224, 224))/255.0
+    #resized_image =np.reshape(resized_image,(224,224,3))
+    return resized_image
+    
+    
+def predict(image_path,model,top_k):    
+    prediction = model.predict(np.expand_dims(image_path, axis=0))
+    result_values, result_indices = tf.math.top_k(prediction,top_k)
+    classes = [class_names[str(element)] for element in result_indices.numpy()[0]]
+    return result_values.numpy()[0], classes
